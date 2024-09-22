983fd4ee31a8a3c8e70998676ee5342b976712b9
Cristina Su Lam <cristina.sulam@mail.utoronto.ca>
2024-09-22 16:10:44 -0400

    Downloaded Dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,29 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from OpenData Toronto.
+# Author: Cristina Su Lam
+# Date: 21 September 2024
+# Contact: cristina.sulam@utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Get all the tickets data
+raw_tickets_data <- list_package_resources("police-annual-statistical-report-tickets-issued") |>
+  filter(name == "Tickets Issued.csv") |>
+  get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_tickets_data, "data/raw_data/raw_tickets_data.csv") 
+
+
 
          
